e17b7791ed56f96bc90e0c7d738a60c3ba42f24b
Tim Kersey <t@k-t.im>
2016-08-29 12:00:35 -0700

    Use default session configuration in Network

--- WhackAPivotRedux/Network.swift
+++ WhackAPivotRedux/Network.swift
@@ -2,7 +2,7 @@ import UIKit
 
 struct Network: NetworkType {
     func request(with request: URLRequest, success: @escaping (Data) -> Void, failure: @escaping (Error) -> Void) {
-        URLSession().dataTask(with: request) { data, urlResponse, error in
+        URLSession(configuration: URLSessionConfiguration.default).dataTask(with: request) { data, response, error in
             if let data = data { success(data) }
             else if let error = error { failure(error) }
         }.resume()
